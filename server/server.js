9f0266046b554abc97a1330399b2d612d54f73df
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2025-11-13 01:22:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins=['http://localhost:5173','https://authentication-system-virid.vercel.app']
+const allowedOrigins=['http://localhost:5173','https://authentication-system-five-khaki.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
