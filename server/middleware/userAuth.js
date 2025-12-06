ecd62d3f0b7ce280dfb9a6ca261b557ffebfe96e
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2025-12-06 12:03:00 +0530

    Update userAuth middleware to retrieve full user data
    Refactor user authentication middleware to fetch full user document from the database.

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,48 +1,5 @@
-// import jwt from "jsonwebtoken";
-
-// const userAuth = async (req, res, next)=>{
-//     const {token} = req.cookies;
-//     if(!token) {
-//         return res.json({success: false, message: 'Not Authorized.Login Again'})
-//     }
-//     try {
-//         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-//         if (tokenDecode.id) {
-//             req.body.userId = tokenDecode.id
-//         }else{
-//             return res.json({ success: false, message: 'Not Authorized.Login Again' });
-//         }
-//         next();
-//     } catch (error) {
-//         res.json({ success: false, message: error.message });
-//     }
-// }
-// export default userAuth;
-// // userAuth.js
-// import jwt from "jsonwebtoken";
-
-// const userAuth = async (req, res, next) => {
-//     const { token } = req.cookies;
-//     if (!token) {
-//         return res.json({ success: false, message: 'Not Authorized. Login Again' });
-//     }
-//     try {
-//         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-//         if (tokenDecode.id) {
-//             req.user = { id: tokenDecode.id }; // store user info here
-//         } else {
-//             return res.json({ success: false, message: 'Not Authorized. Login Again' });
-//         }
-//         next();
-//     } catch (error) {
-//         res.json({ success: false, message: error.message });
-//     }
-// };
-
-// export default userAuth;
-
 import jwt from "jsonwebtoken";
-import userModel from "../models/userModel.js"; // make sure path is correct
+import userModel from "../models/userModel.js";
 
 const userAuth = async (req, res, next) => {
   const { token } = req.cookies;
@@ -52,14 +9,11 @@ const userAuth = async (req, res, next) => {
 
   try {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-    // fetch full Mongoose user document
     const user = await userModel.findById(tokenDecode.id);
     if (!user) {
       return res.json({ success: false, message: "User not found" });
     }
-
-    req.user = user; // ✅ now you have full doc, including .save()
+    req.user = user;
     next();
   } catch (error) {
     res.json({ success: false, message: error.message });
