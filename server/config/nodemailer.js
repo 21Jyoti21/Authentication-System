8ff91fe08096c4e5e22b095320ff7d11ef8c43d2
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2025-12-06 12:07:06 +0530

    Refactor nodemailer configuration for clarity

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,23 +1,11 @@
-// backend/nodemailer.js
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // use TLS, not SSL
+  secure: false,
   auth: {
-    user: process.env.SENDER_EMAIL,       // your Gmail
-    pass: process.env.GMAIL_APP_PASSWORD, // app password generated in Google
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.GMAIL_APP_PASSWORD,
   },
 });
-
-// import nodemailer from 'nodemailer'
-// const transporter = nodemailer.createTransport({
-//     host:'smtp-relay.brevo.com',
-//     port:587,
-//     auth:{
-//         user:process.env.SMTP_USER,
-//         pass:process.env.SMTP_PASS,
-//     }
-// });
-// export default transporter;
